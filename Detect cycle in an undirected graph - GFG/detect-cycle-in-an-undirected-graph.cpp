3c4d143f970e731d5b6f364e524909e10110839d
Sakshi19Naruka <105409845+Sakshi19Naruka@users.noreply.github.com>
2022-07-13 12:08:36 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -5,77 +5,25 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
+  // Using DFS
+  bool checkForCycle(int node, int parent, vector<int> &vis, vector<int> adj[]){
+     vis[node]=1;
+     for(auto it:adj[node]){
+         if(vis[it]==0){
+             if(checkForCycle(it, node, vis, adj))  return true;
+         }
+         else if(it != parent)  return true;
+     }
+     return false;
+  }
   
-//   bool bfs(int i, int V, vector<int> &vis, vector<int> adj[]){
-//         queue<pair<int, int>> q;
-//         q.push({i, -1});
-//         while(!q.empty()){
-//             int curr = q.front().first;
-//             int prev = q.front().second;
-//             vis[curr] = 1;
-//             q.pop();
-//             for(auto it : adj[curr]){
-//                 if(!vis[it]){
-//                     vis[it] = 1;
-//                     q.push({it, curr});
-//                 }else if(vis[it] == 1 && prev != it){
-//                     return true;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-    
-    
-//     bool isCycle(int V, vector<int> adj[]) {
-//         // Code here
-//         vector<int> vis(V, 0);
-//         for(int i = 0; i < V; i++){
-//             if(!vis[i]){
-//                 if(bfs(i, V, vis, adj)){
-//                     return true;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-// };
-  
-  bool checkForCycle(int s, int V, vector<int> adj[], vector<int> &visited)
-    {
-        // Create a queue for BFS
-        queue<pair<int, int>> q;
-        visited[s] = 1;
-        q.push({s, -1});
-        while (!q.empty())
-        {
-            int node = q.front().first;
-            int par = q.front().second;
-            
-            q.pop();
- 
-            for (auto it : adj[node])
-            {
-                if (!visited[it])
-                {
-                    visited[it] = 1;
-                    q.push({it, node});
-                }
-                else if (par != it)
-                    return true;
-            }
-        }
-        return false;
-    }
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
-        vector<int> vis(V+1, 0);
-        int i=0;
-        for(i=0; i<V; i++){    //for more than one component
+        vector<int> vis(V+1,0);
+        for(int i=0; i<V; i++){
             if(!vis[i]){
-                if(checkForCycle(i, V, adj, vis))
-                  return true;
+                if(checkForCycle(i, -1, vis, adj)) return true;
             }
         }
         return false;
