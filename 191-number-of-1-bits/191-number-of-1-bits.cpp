46d038d24a5191d3b06af04ee7dbfd03c82588af
Sakshi19Naruka <105409845+Sakshi19Naruka@users.noreply.github.com>
2022-09-20 15:29:44 +0530

    Time: 0 ms (100.00%), Space: 6 MB (47.51%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int count = 0; // count of 1's in binary representation
         
-        while(n!=0){
-            // we have to check the last bit 
-            if(n&1){      
-                count++;
-            }
-            n = n>>1;  // right shift till it will not become 0
+        
+        int cnt=0;  // count of set bit
+        while(n>0){  // iterate until all bits are traversed
+			++cnt;
+            n=n&(n-1); // change the first set bit from right to 0
         }
-        return count;
+        return cnt;
     }
 };
