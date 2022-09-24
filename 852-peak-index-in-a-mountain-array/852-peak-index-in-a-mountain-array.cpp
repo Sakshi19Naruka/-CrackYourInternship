41513695ccee586b6aaa0888d52a01cc4719f568
Sakshi19Naruka <105409845+Sakshi19Naruka@users.noreply.github.com>
2022-09-24 22:08:20 +0530

    Time: 285 ms (11.43%), Space: 59.7 MB (17.29%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int low=0, high=arr.size()-1;
+        int mid = low + (high-low)/2;
+        
+        // binary search approach
         
         while(low<high){
-            if(arr[low]<arr[low+1]){
-                low++;
+            if(arr[mid]<arr[mid+1]){
+                low = mid + 1;
             }
-            else{
-                return low;
+            else{    // bcoz mid hi peak hua to .....isiliye high = mid-1 nhi kiya high=mid kiya .LOVE BABBAR LECTURE 12 
+                high = mid;
             }
-            if(arr[high]<arr[high-1]){
-                high--;
-            }
+           mid = low + (high-low)/2;
         }
         return low;
     }
