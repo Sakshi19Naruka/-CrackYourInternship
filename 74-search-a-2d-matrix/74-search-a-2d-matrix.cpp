3821e61475a9b6fb082100109217408952b7c65c
Sakshi19Naruka <105409845+Sakshi19Naruka@users.noreply.github.com>
2022-10-09 14:42:41 +0530

    Time: 4 ms (82.29%), Space: 9.6 MB (5.07%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,22 +1,40 @@
-// Brute force
-
 class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-		// running a forloop till size of matrix
-        for(int i  = 0; i<matrix.size(); ++i)
-        {
-		// nested for for ietrating each row element
-            for(int j = 0; j<matrix[0].size(); ++j)
-            {
-				// if found return true
-                if(matrix[i][j] == target)
-                    return true;
+private:
+    bool binarySearch(int row, int col, vector<vector<int>>& arr, int target){
+        int s=0, e=col-1;
+        int mid = s+(e-s)/2;
+        
+        while(s<=e){
+            if(arr[row][mid] == target)
+                return true;
+            else if(arr[row][mid] > target){
+                e = mid-1;
             }
+            else{
+                s = mid+1;
+            }
+             mid = s+(e-s)/2;
         }
-		// after traversal if not found
-		// return false
         return false;
     }
-	
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        bool ans;
+        
+        if(target<matrix[0][0] || target>matrix[row-1][col-1])
+                return false;
+        
+        for(int i=0; i<row; i++){
+           
+            if(target<=matrix[i][col-1] && target>=matrix[i][0])
+                ans = binarySearch(i, col, matrix, target);
+            
+            else{
+                continue;
+            }
+        }
+        return ans;
+    }
 };
