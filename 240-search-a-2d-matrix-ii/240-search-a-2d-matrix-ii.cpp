31e4e005a6d009f1286662a2d8dc76d7efa27b58
Sakshi19Naruka <105409845+Sakshi19Naruka@users.noreply.github.com>
2022-10-09 15:46:20 +0530

    Time: 191 ms (50.71%), Space: 14.8 MB (88.06%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,43 +1,23 @@
 class Solution {
-private:
-    bool binarySearch(int row, int col, vector<vector<int>>& arr, int target){
-        int s=0, e=col-1;
-        int mid = s+(e-s)/2;
-        
-        while(s<=e){
-            if(arr[row][mid] == target)
-                return true;
-            else if(arr[row][mid] > target){
-                e = mid-1;
-            }
-            else{
-                s = mid+1;
-            }
-             mid = s+(e-s)/2;
-        }
-        return false;
-    }
-    
+    // Love babbar bhaiya approach -->1st row k last col wale element se start kiya h 
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row = matrix.size();
         int col = matrix[0].size();
-        bool ans;
         
-        if(target<matrix[0][0] || target>matrix[row-1][col-1])
-                return false;
+        //1st row k last col wale element se start kiya h
+        int rowIndex = 0;
+        int colIndex = col-1;
         
-        for(int i=0; i<row; i++){
-            if(target<=matrix[i][col-1] && target>=matrix[i][0]){
-                if(binarySearch(i, col, matrix, target))
-                    return true;
-                else
-                    continue;
-            }
-                
-            else{
-                continue;
-            }
+        while(rowIndex<row && colIndex>=0){
+            int element = matrix[rowIndex][colIndex];
+            
+            if(element == target)
+                return true;
+            else if(element < target) //it means us row me to exist nhi kr rha hoga
+                rowIndex++;
+            else                      //it means us col me to exist nhi kr rha hoga
+                colIndex--;
         }
         return false;
     }
