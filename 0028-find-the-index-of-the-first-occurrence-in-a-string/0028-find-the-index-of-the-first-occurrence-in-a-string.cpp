93721e48f39a91f8dbda2df2dcd9d3dbf0984a32
Sakshi19Naruka <105409845+Sakshi19Naruka@users.noreply.github.com>
2022-10-15 10:07:55 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (61.41%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int h = haystack.size();
-        int n = needle.size();
-        
-        if(h<n) 
-            return -1;
-        
-        string str;
-        for(int i=0; i<=h-n; i++){
-            str = haystack.substr(i,n);   //[)
-            if(str == needle)
-                return i;
-        }
-          return -1;  
+        int i=0,j=0;
+        while(i<haystack.size() && j<needle.size())
+        {
+            if(haystack[i]==needle[j])
+            { // when there is a match then increase both the pointers
+                i++;
+                j++;
+                if(j==needle.size()) // if the whole pattern has been matched then return
+                    return i-needle.size();  
+            }
+            else
+            { 
+                i=i-j+1;  // Why we did not write i++ bcoz it will fail in eg. butstad and sad
+                j=0; 
+            }            
+        }   
+        return -1;
     }
 };
