c0410be109ed382ac10151cd316357ba545aa523
Sakshi19Naruka <105409845+Sakshi19Naruka@users.noreply.github.com>
2022-12-05 11:28:31 +0530

    Time: 246 ms (79.20%), Space: 93.7 MB (7.09%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,46 +1,64 @@
-
-// T.C - O(N)
-
+// concept same as last submission but way of writing is different 
 
 class Solution {
+private:
+    vector<int> nextSmallerElement(vector<int> arr, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for(int i=n-1; i>=0 ; i--) {
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr)
+            {
+                s.pop();
+            }
+            //ans is stack ka top
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    
+    vector<int> prevSmallerElement(vector<int> arr, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for(int i=0; i<n; i++) {
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr)
+            {
+                s.pop();
+            }
+            //ans is stack ka top
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans; 
+    }
+    
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        //base case
-        if(n==0)
-            return 0;
+        int n= heights.size();
         
-        int maxArea = 0;
-        vector<int> left(n);  //left boundary
-        vector<int> right(n);   //right boundary
-        
-        left[0] = -1;
-        right[n-1] = n;
-        
-        for(int j=1; j<n; j++){
-            int prev = j-1;   //previous for comparing the heights
+        vector<int> next(n);
+        next = nextSmallerElement(heights, n);
             
-            while(prev>=0 && heights[prev]>=heights[j]){
-                prev = left[prev];
-            }
-            left[j] = prev;
-        }
+        vector<int> prev(n);
+        prev = prevSmallerElement(heights, n);
         
-        //Similarly we will do for right
-        for(int j=n-2; j>=0; j--){
-            int prev = j+1;   //previous for comparing the heights
+        int area = INT_MIN;
+        for(int i=0; i<n; i++) {
+            int l = heights[i];
             
-            while(prev<n && heights[prev]>=heights[j]){
-                prev = right[prev];
+            if(next[i] == -1) {
+                next[i] = n;
             }
-            right[j] = prev;
+             int b = next[i] - prev[i] - 1;
+            int newArea = l*b;
+            area = max(area, newArea);
         }
-        
-        //once we have these two arrays fill, then we need width and height
-        for(int i=0; i<n; i++){
-            int width = right[i] - left[i] - 1;
-            maxArea = max(maxArea, heights[i]*width);
-        }
-        return maxArea;
+        return area;
     }
 };
