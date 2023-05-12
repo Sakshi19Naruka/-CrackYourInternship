c2aff1596438d1c1f5a6d872ba809f70e8ea7b2e
Sakshi19Naruka <105409845+Sakshi19Naruka@users.noreply.github.com>
2023-05-12 18:51:00 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
@@ -8,29 +8,24 @@ using namespace std;
 class Solution
 {
     public:
-    void sort012(int a[], int n)
+    void sort012(int arr[], int n)
     {
-        // code here
-        int cnt0=0, cnt1=0, cnt2=0;
-        for(int i=0; i<n; i++){
-            if(a[i]==0)
-                cnt0++;
-            else if(a[i]==1)
-                cnt1++;
-            else
-                cnt2++;
+        // code here 
+        int low=0, mid=0, high=n-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else if(arr[mid]==2){
+                swap(arr[mid], arr[high]);
+                high--;
+            }
         }
-        
-        for(int i=0; i<cnt0; i++){
-            a[i]=0;
-        }
-        for(int i=cnt0; i<(cnt0 + cnt1); i++){
-            a[i]=1;
-        }
-        for(int i=(cnt0 + cnt1); i<n; i++){
-            a[i]=2;
-        }
-        
     }
     
 };
